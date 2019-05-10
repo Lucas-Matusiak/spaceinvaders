1e76a74ae2d0fa6574b968cced2568aa83768bfd
Lucas <lucassmatusiak@gmail.com>
2019-05-10 10:05:04 +0200

    5.2 Quid de la validité de la position de tir du Missile dans l'espace

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Missile.java
@@ -0,0 +1,14 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class Missile extends Sprite {
+
+
+
+	public Missile(Dimension dimensionMissile, Position positionOrigineMissile, int vitesseMissile) {
+		super(dimensionMissile,positionOrigineMissile,vitesseMissile);
+		// TODO Auto-generated constructor stub
+	}
+	
+   
+
+}
